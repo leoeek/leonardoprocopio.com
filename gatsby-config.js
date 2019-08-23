d152ab49ee4ea1c5d4b5edb42331069a0fe460b6
Unknown <leonardolp@gmail.com>
2019-08-23 15:50:27 -0300

    Adicionando analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -162,7 +162,7 @@ if (process.env.CONTEXT === `production`) {
   cfg.plugins.push({
     resolve: `gatsby-plugin-google-analytics`,
     options: {
-      trackingId: `UA-145475954-1`,
+      trackingId: `UA-25369842-1`,
     },
   });
 }
